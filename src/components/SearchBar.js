d1988edc937f151e2495c46c93ab0f386ad31ac8
Liam Cavens <liamcavens@googlemail.com>
2018-08-12 00:13:30 +0100

    got fetch url working

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,34 @@ import React, { Component } from "react";
 import "./SearchBar.css";
 
 export default class SearchBar extends Component {
+  state = {
+    userInput: ""
+  };
+
+  handleUserInput = e => {
+    const userInput = e.target.value;
+    this.setState({ userInput });
+  };
+
   render() {
     return (
       <div className="searchbar">
-        <p id='app-name-logo'>Flickr</p>
-        <form className="searchform" action="" method="post">
-          <input id="search" type="search" name="search" />
-          <button type="submit">Search</button>
-        </form>
+        <p id="app-name-logo">Flickr</p>
+        <div className="searchform">
+          <input
+            id="search"
+            type="search"
+            name="search"
+            onChange={this.handleUserInput}
+          />
+          <button
+            onClick={() => {
+              this.props.handleSearchClick(this.state.userInput);
+            }}
+          >
+            Search
+          </button>
+        </div>
       </div>
     );
   }
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
-import SearchBar from '../components/SearchBar';
-import ImageBox from '../components/ImageBox';
+import SearchBar from "../components/SearchBar";
+import ImageBox from "../components/ImageBox";
 
 class MainBox extends Component {
+  state = {
+    images: []
+  };
+
+  handleSearchClick = searchWord => {
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=4b23ff6de8623a81e2c7c2fa196d5d8a&format=json&format=json&nojsoncallback=true&tags=${searchWord}&per_page=20`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+        const images = json.photos.photo.map((image, index) => ({
+          id: image.id,
+          secred: image.secret,
+          server: image.server,
+          farm: image.farm
+        }))
+      }
+  };
+
+  fetchMoreImages = () => {
+    const self = this;
+
+    // let url = #;
+  };
+
   render() {
     return (
       <div>
         <header>
-            <SearchBar />
-            <ImageBox />
+          <SearchBar handleSearchClick={this.handleSearchClick} />
+          <ImageBox images={this.state.images} />
         </header>
       </div>
     );
